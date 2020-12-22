8ab9ee8f1935ad4a29af92d0709fe3931f5647ee
Hideaki Matsunami <hideaki.matsunami@esm.co.jp>
2020-12-22 19:42:10 +0900

    リバースプロキシ用のapacheを構成中

---
+++ proxy/provision.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo --- apache configuration ---
+yum -y install httpd
+rm -rf /etc/httpd/conf.d/
+cp /vagrant/proxy/httpd.conf /etc/httpd/conf/
+sudo systemctl enable httpd.service
+sudo systemctl start httpd.service
+
+echo --- setup firewalld ---
+systemctl start firewalld
+firewall-cmd --zone=public --add-port=10080/tcp --permanent
+firewall-cmd --reload
