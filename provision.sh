a6977c86f254a3ad819308bf38b03fe81c18e741
Hideaki Matsunami <hideaki.matsunami@esm.co.jp>
2020-12-22 11:54:19 +0900

    serviceのユニット定義を作成

--- provision.sh
+++ provision.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
 echo --- install jdk ---
-
-yum -y install java-11-openjdk java-11-openjdk-devel
-export JAVA_HOME=$(dirname $(dirname $(readlink $(readlink $(which javac)))))
-export PATH=\$PATH:\$JAVA_HOME/bin 
+# yum -y install java-11-openjdk java-11-openjdk-devel
+# export JAVA_HOME=$(dirname $(dirname $(readlink $(readlink $(which javac)))))
+# export PATH=\$PATH:\$JAVA_HOME/bin 
 
 echo --- disable firewalld ---
-
 sudo systemctl stop firewalld
+
+echo --- register service ---
+cp /vagrant/demoapp.service /etc/systemd/system
+
+echo --- start service ---
+systemctl daemon-reload
+systemctl start demoapp.service
+
+echo --- ssl certificate ---
+
+echo --- restart apache ---
