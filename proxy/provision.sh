12c8d97b8e0e5caf6731736a93205d248e25620d
Hideaki Matsunami <hideaki.matsunami@esm.co.jp>
2020-12-23 19:09:37 +0900

    リバースプロキシを構成中
    httpdを起動して1分くらい経つと`AH00170: caught SIGWINCH, shutting down gracefully`エラーが出て
    終了してしまう。
    
    なぜかは分からない...

--- proxy/provision.sh
+++ proxy/provision.sh
@@ -7,7 +7,9 @@ cp /vagrant/proxy/httpd.conf /etc/httpd/conf/
 sudo systemctl enable httpd.service
 sudo systemctl start httpd.service
 
-echo --- setup firewalld ---
-systemctl start firewalld
-firewall-cmd --zone=public --add-port=10080/tcp --permanent
-firewall-cmd --reload
+# echo --- setup firewalld ---
+# systemctl start firewalld
+# firewall-cmd --zone=public --add-port=10080/tcp --permanent
+# firewall-cmd --reload
+
+setenforce 0
