c0a99ba2a6d0d6dea087c9f61543193583483b6c
Hideaki Matsunami <hideaki.matsunami@esm.co.jp>
2020-12-24 17:25:28 +0900

    プロビジョニングの内容を変更

--- proxy/provision.sh
+++ proxy/provision.sh
@@ -2,14 +2,11 @@
 
 echo --- apache configuration ---
 yum -y install httpd
-rm -rf /etc/httpd/conf.d/
-cp /vagrant/proxy/httpd.conf /etc/httpd/conf/
+# rm -rf /etc/httpd/conf.d/
+# cp /vagrant/proxy/httpd.conf /etc/httpd/conf/
 sudo systemctl enable httpd.service
 sudo systemctl start httpd.service
 
 # echo --- setup firewalld ---
-# systemctl start firewalld
-# firewall-cmd --zone=public --add-port=10080/tcp --permanent
-# firewall-cmd --reload
-
+sudo systemctl stop firewalld
 setenforce 0
